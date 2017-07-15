6170e991d31ea46ea88791b8db7a47e65df6dd85
Asanka Dharma <mowgl1@Asankas-Mac-Pro.JIBA-network>
2017-07-14 23:22:31 -0700

    clean up

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -6,23 +6,22 @@ from bs4 import BeautifulSoup
 """pull superpac info into page object"""
 page = requests.get("https://www.opensecrets.org/pacs/superpacs.php")
 
-print "contents of page object... fresh from the internets"
-print page.content
+print ("Hello World!")
+print ("contents of page object... fresh from the interwebs")
+time.sleep(10)
+print (page.content)
 
 """parse the contents using BeautifulSoup, store in soup instance, parse using html)"""
 soup = BeautifulSoup(page.content, 'html.parser')
 
-print "**************"
-print soup.content
-help('BeautifulSoup')
-
-time.sleep(1)
+print ("**************")
+print ("soup.prettify")
+time.sleep(10)
 print (soup.prettify())
-time.sleep(5)
-print "soup children"
+
+
+print ("soup children")
+time.sleep(10)
 list(soup.children)
-
-time.sleep(5)
-
-print [type(item) for item in list(soup.children)]
+print ([type(item) for item in list(soup.children)])
 
